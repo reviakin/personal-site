bc3174e1ba1d3483251fb2f94da92e14879f3db6
reviakin <reviakindi@gmail.com>
2019-06-06 22:03:16 +0300

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import Header from "./components/Header";
 
 function App() {
     return (
         <div>
             <h1>Reviakin Dmitry</h1>
+            <Header />
         </div>
     );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Abount me
+                </li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    )
+}
