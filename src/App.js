1f3a416a75c9edb127e97ac9b847d7df6fe46081
reviakin <reviakindi@gmail.com>
2019-06-06 22:00:26 +0300

    do basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+function App() {
+    return (
+        <div>
+            <h1>Reviakin Dmitry</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"))
