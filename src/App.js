4cdf03542f7861d3eca4c55517e5b8cbbe13f436
reviakin <reviakindi@gmail.com>
2019-06-08 22:07:07 +0300

    add @reach/router in prj

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Router } from '@reach/router'
 
 import Header from "./components/Header"
 import AboutMe from "./components/AboutMe"
