cd379e0a8c0d815b0b05f08750daab4d6e75431b
reviakin <reviakindi@gmail.com>
2019-06-08 22:24:03 +0300

    do open new blank when u've clicked <a/> in Contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -7,22 +7,34 @@ export default function Contacts() {
                 <h2>Contacts:</h2>
                 <ul>
                     <li>
-                        <a href="https://www.instagram.com/reviakin_/">
+                        <a
+                            href="https://www.instagram.com/reviakin_/"
+                            target="_blank"
+                            rel='noreferrer noopener'>
                             Instagram
                         </a>
                     </li>
                     <li>
-                        <a href="https://vk.com/id539526023">
+                        <a
+                            href="https://vk.com/id539526023"
+                            target="_blank"
+                            rel='noreferrer noopener'>
                             VK
                         </a>
                     </li>
                     <li>
-                        <a href="https://t.me/reviak1n">
+                        <a
+                            href="https://t.me/reviak1n"
+                            target="_blank"
+                            rel='noreferrer noopener'>
                             Telegram
                         </a>
                     </li>
                     <li>
-                        <a href="mailto:reviakindi@gmail.com?subject=Your personal site is amazing">
+                        <a
+                            href="mailto:reviakindi@gmail.com?subject=Your personal site is amazing"
+                            target="_blank"
+                            rel='noreferrer noopener'>
                             Email
                         </a>
                     </li>
