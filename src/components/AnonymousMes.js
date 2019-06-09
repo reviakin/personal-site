f876998434749c73db256b4186694349c332f65b
reviakin <reviakindi@gmail.com>
2019-06-09 21:41:47 +0300

    get message from firebase

--- src/components/AnonymousMes.js
+++ src/components/AnonymousMes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function AnonymousMes({
@@ -11,6 +11,25 @@ export default function AnonymousMes({
     function handleChangeMes(event) {
         setMessage(event.target.value)
     }
+
+    async function getMessageFromServer() {
+        var { data } = await axios.get("https://reviakinpersonalsite.firebaseio.com/messages.json")
+
+        var dataWithIterator = {
+            ...data,
+            *[Symbol.iterator]() {
+                for (let key of Object.keys(this)) {
+                    yield this[key]
+                }
+            }
+        }
+
+        setMessages([...dataWithIterator] || [])
+    }
+
+    useEffect(() => {
+        getMessageFromServer()
+    }, [])
 
     return (
         <div>
@@ -26,6 +45,11 @@ export default function AnonymousMes({
                 </label>
                 <button> Submit</button>
             </form>
+            <div>
+                {messages.length > 0 && messages.map(function renderMes({ message, time }) {
+                    return <p key={time}>{`${message} - ${time}`}</p>
+                })}
+            </div>
         </div >
     )
 }
