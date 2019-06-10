18d1ed52d3cfdab3bce402cac39e18807ac1afa5
reviakin <reviakindi@gmail.com>
2019-06-10 10:45:24 +0300

    delete spare div in Contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,43 +3,41 @@ import React from 'react'
 export default function Contacts() {
     return (
         <div>
-            <div>
-                <h2>Contacts:</h2>
-                <ul>
-                    <li>
-                        <a
-                            href="https://www.instagram.com/reviakin_/"
-                            target="_blank"
-                            rel='noreferrer noopener'>
-                            Instagram
+            <h2>Contacts:</h2>
+            <ul>
+                <li>
+                    <a
+                        href="https://www.instagram.com/reviakin_/"
+                        target="_blank"
+                        rel='noreferrer noopener'>
+                        Instagram
                         </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://vk.com/id539526023"
-                            target="_blank"
-                            rel='noreferrer noopener'>
-                            VK
+                </li>
+                <li>
+                    <a
+                        href="https://vk.com/id539526023"
+                        target="_blank"
+                        rel='noreferrer noopener'>
+                        VK
                         </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://t.me/reviak1n"
-                            target="_blank"
-                            rel='noreferrer noopener'>
-                            Telegram
+                </li>
+                <li>
+                    <a
+                        href="https://t.me/reviak1n"
+                        target="_blank"
+                        rel='noreferrer noopener'>
+                        Telegram
                         </a>
-                    </li>
-                    <li>
-                        <a
-                            href="mailto:reviakindi@gmail.com?subject=Your personal site is amazing"
-                            target="_blank"
-                            rel='noreferrer noopener'>
-                            Email
+                </li>
+                <li>
+                    <a
+                        href="mailto:reviakindi@gmail.com?subject=Your personal site is amazing"
+                        target="_blank"
+                        rel='noreferrer noopener'>
+                        Email
                         </a>
-                    </li>
-                </ul>
-            </div>
+                </li>
+            </ul>
         </div>
     )
 }
