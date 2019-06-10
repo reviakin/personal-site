065d9ad8a9141b6334032e023eb33c508656a8d3
reviakin <reviakindi@gmail.com>
2019-06-10 11:04:19 +0300

    delete name fragment

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,28 @@ import { Link } from '@reach/router'
 export default function Header() {
     return (
         <div>
-            <div>
-                <h1>Reviakin Dmitry</h1>
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">
-                            Abount me
+            <ul>
+                <li>
+                    <Link to="/">
+                        Abount me
                     </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            My project
+                </li>
+                <li>
+                    <Link to="/">
+                        My project
                     </Link>
-                    </li>
-                    <li>
-                        <Link to="/contacts">
-                            Contacts
+                </li>
+                <li>
+                    <Link to="/contacts">
+                        Contacts
                     </Link>
-                    </li>
-                    <li>
-                        <Link to="anonymous">
-                            Anonymous
+                </li>
+                <li>
+                    <Link to="anonymous">
+                        Anonymous
                     </Link>
-                    </li>
-                </ul>
-            </div>
+                </li>
+            </ul>
         </div>
     )
 }
