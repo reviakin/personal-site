3e52bae65091ccc1899f83692598e9b701e4887c
reviakin <reviakindi@gmail.com>
2019-06-10 15:36:05 +0300

    do contacts list render dynamic

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,55 +5,46 @@ export default function Contacts() {
         <div>
             <h3>Contacts:</h3>
             <ul>
-                <li>
-                    <a
-                        href="https://www.instagram.com/reviakin_/"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        Instagram
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://vk.com/id539526023"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        VK
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://t.me/reviak1n"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        Telegram
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="mailto:reviakindi@gmail.com?subject=Your personal site is amazing"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        Email
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.last.fm/user/dmitryRev"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        last.fm
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://github.com/reviakin"
-                        target="_blank"
-                        rel='noreferrer noopener'>
-                        GitHub
-                    </a>
-                </li>
+                {contacts.map(function renderContacts({ link, name }) {
+                    return (
+                        <li key={link}>
+                            <a
+                                href={link}
+                                target="_blank"
+                                rel='noreferrer noopener'>
+                                {name}
+                            </a>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
 }
+
+var contacts = [
+    {
+        "link": "https://www.instagram.com/reviakin_/",
+        "name": "Instagram"
+    },
+    {
+        "link": "https://vk.com/id539526023",
+        "name": "VK"
+    },
+    {
+        "link": "https://t.me/reviak1n",
+        "name": "Telegram"
+    },
+    {
+        "link": "mailto:reviakindi@gmail.com?subject=Your personal site is amazing",
+        "name": "Email"
+    },
+    {
+        "link": "https://www.last.fm/user/dmitryRev",
+        "name": "last.fm"
+    },
+    {
+        "link": "https://github.com/reviakin",
+        "name": "GitHub"
+    }
+]
