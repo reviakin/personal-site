e3b43234e86c9db79dd71732f7fbd0377df21f4e
reviakin <reviakindi@gmail.com>
2019-06-10 15:10:31 +0300

    add ErrorBoundary in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Router } from '@reach/router'
 
+import ErrorBoundary from './components/ErrorBoundary'
 import Header from "./components/Header"
 import AboutMe from "./components/AboutMe"
 import Contacts from "./components/Contacts"
@@ -9,21 +10,23 @@ import AnonymousMes from './components/AnonymousMes'
 
 function App() {
     return (
-        <div className='container'>
-            <div className='title'>
-                <h1>Reviakin Dmitry</h1>
+        <ErrorBoundary>
+            <div className='container'>
+                <div className='title'>
+                    <h1>Reviakin Dmitry</h1>
+                </div>
+                <div className='navigation'>
+                    <Header />
+                </div>
+                <div className='content'>
+                    <Router>
+                        <AboutMe path="/" />
+                        <Contacts path="/contacts" />
+                        <AnonymousMes path="/anonymous" />
+                    </Router>
+                </div>
             </div>
-            <div className='navigation'>
-                <Header />
-            </div>
-            <div className='content'>
-                <Router>
-                    <AboutMe path="/" />
-                    <Contacts path="/contacts" />
-                    <AnonymousMes path="/anonymous" />
-                </Router>
-            </div>
-        </div>
+        </ErrorBoundary>
     );
 };
 
