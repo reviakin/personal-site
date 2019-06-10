cc00a624855c0dfd40bc7009e39c7d7b994c9535
reviakin <reviakindi@gmail.com>
2019-06-10 10:43:17 +0300

    add name fragment in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,33 @@ import { Link } from '@reach/router'
 export default function Header() {
     return (
         <div>
-            <ul>
-                <li>
-                    <Link to="/">
-                        Abount me
+            <div>
+                <h1>Reviakin Dmitry</h1>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            Abount me
                     </Link>
-                </li>
-                <li>
-                    <Link to="/">
-                        My project
+                    </li>
+                    <li>
+                        <Link to="/">
+                            My project
                     </Link>
-                </li>
-                <li>
-                    <Link to="/contacts">
-                        Contacts
+                    </li>
+                    <li>
+                        <Link to="/contacts">
+                            Contacts
                     </Link>
-                </li>
-                <li>
-                    <Link to="anonymous">
-                        Anonymous
+                    </li>
+                    <li>
+                        <Link to="anonymous">
+                            Anonymous
                     </Link>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
