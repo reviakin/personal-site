e1ccb997452861e4c3554c1581703ee52277f36a
reviakin <reviakindi@gmail.com>
2019-06-11 21:35:48 +0300

    change name form in AboutMe component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
         <div>
             <h3>Who Am I?</h3>
             <p>
-                My name is Dima Reviakin. I'm presently (as of writing) study React js development.
+                My name is Dima. I'm presently (as of writing) study React js development.
                 My biggest passions in life are people and experiences. I live in Pskov.
             </p>
         </div>
