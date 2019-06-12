9a3451e5d83448f67549bcba5b21a1f9406062e4
reviakin <reviakindi@gmail.com>
2019-06-12 12:19:59 +0300

    add active Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,51 @@
 import React from 'react'
 import { Link } from '@reach/router'
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div>
             <ul>
                 <li>
-                    <Link to="/">
+                    <Link {...props} getProps={({ isCurrent }) => {
+                        return {
+                            style: {
+                                color: isCurrent ? "#E2C044" : ""
+                            }
+                        }
+                    }} to="/">
                         Abount me
                     </Link>
                 </li>
                 <li>
-                    <Link to="/projects">
+                    <Link {...props} getProps={({ isCurrent }) => {
+                        return {
+                            style: {
+                                color: isCurrent ? "#E2C044" : ""
+                            }
+                        }
+                    }} to="/projects">
                         My project
                     </Link>
                 </li>
                 <li>
-                    <Link to="/contacts">
+                    <Link {...props} getProps={({ isCurrent }) => {
+                        return {
+                            style: {
+                                color: isCurrent ? "#E2C044" : ""
+                            }
+                        }
+                    }} to="/contacts">
                         Contacts
                     </Link>
                 </li>
                 <li>
-                    <Link to="anonymous">
+                    <Link {...props} getProps={({ isCurrent }) => {
+                        return {
+                            style: {
+                                color: isCurrent ? "#E2C044" : ""
+                            }
+                        }
+                    }} to="anonymous">
                         Anonymous
                     </Link>
                 </li>
