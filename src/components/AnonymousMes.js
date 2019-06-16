a6a6e532886db2942afcb95fcb9633a45729c0d4
reviakin <reviakindi@gmail.com>
2019-06-16 14:57:58 +0300

    do return [messages] in getMessageFromServer func

--- src/components/AnonymousMes.js
+++ src/components/AnonymousMes.js
@@ -23,8 +23,7 @@ export default function AnonymousMes({
                 }
             }
         }
-
-        setMessages([...dataWithIterator] || [])
+        return ([...dataWithIterator], [])
     }
 
     useEffect(() => {
