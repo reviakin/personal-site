e29e99aa00fc3eeb17610a76314755526e670648
reviakin <reviakindi@gmail.com>
2019-07-05 22:12:43 +0300

    add root render

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ var express = require('express')
 
 var app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.get('/', (req,res,next)=> {
+    res.render('index')
+})
+
 app.listen(3333)
