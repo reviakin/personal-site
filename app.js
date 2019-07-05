4125d82089fe8ed3b36f7a088943757d72464ea1
reviakin <reviakindi@gmail.com>
2019-07-05 22:31:15 +0300

    add express static method

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var path = require('path')
+
 var express = require('express')
 
 var app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req,res,next)=> {
     res.render('main')
