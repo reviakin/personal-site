5eadde0fac0780334e1b13e26e0af6ec1af47d0c
reviakin <reviakindi@gmail.com>
2019-08-25 18:58:19 +0300

    add dotenv, add env config, fixed css file reading

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import * as express from 'express'
 import * as path from 'path'
 
 import siteRouter from './site/site.router'
+import config from './config'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(siteRouter)
 app.use('/', (req, res) => {
@@ -16,5 +17,5 @@ app.use('/', (req, res) => {
 })
 
 export const start = () => {
-  app.listen(3000, () => console.log(`server start on 3000`))
+  app.listen(config.port, () => console.log(`server start on ${config.port}`))
 }
